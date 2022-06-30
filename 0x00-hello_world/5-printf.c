852990a2d0fdb36f51ee44510ef67b116c2cf301
ferouzewere <ferouzewere@gmail.com>
2022-06-30 16:06:13 -0700

    a C program that prints word by word

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,11 @@
-#include<stdio.h>
+#include <stdio.h>
 /**
-*Description: main - prints out a string.
-*Return : 0 if success.
-*/
+ * main - Entry point
+ *
+ * Return: Always 0 (Sucess)
+ */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n");
-return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
