24525df1d9661b284788fc280942f91735fd62fb
ferouzewere <ferouzewere@gmail.com>
2022-07-01 13:19:55 -0700

    Print all the letters except q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Prints specific alphabetic letters
+ *
+ * Return: Always (Success)
+ */
+int main(void)
+{
+char alph;
+for (alph = 'a'; alph <= 'z'; alph++)
+{
+if (alph != 'e' && alph != 'q')
+{
+putchar(alph);
+}
+}
+putchar('\n');
+return (0);
+}
