6dbb7b0032282bcd97ebb4063da341039311f71b
ferouzewere <ferouzewere@gmail.com>
2022-07-01 13:11:43 -0700

    prints the alphabet in lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Prints the alphabetic
+ *
+ * Return: Always (Success)
+ */
+int main(void)
+{
+char alph;
+for (alph = 'a'; alph <= 'z'; alph++)
+putchar(alph);
+putchar('\n');
+return (0);
+}
