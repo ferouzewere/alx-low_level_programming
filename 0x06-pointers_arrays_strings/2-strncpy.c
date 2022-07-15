3ec3c9c220511ff407c1116d583a42c68bd79a21
ferouzewere <ferouzewere@gmail.com>
2022-07-14 17:58:50 -0700

    a function that copies a string.

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,41 +1,28 @@
 #include "main.h"
-#include <stdio.h>
-
 /**
- * main - check the code
+ * _strncpy - Copy a string
+ * @dest: The destination value
+ * @src: The source value
+ * @n: The copy limit
  *
- * Return: Always 0.
+ * Return: char value
  */
-int main(void)
+char *_strncpy(char *dest, char *src, int n)
 {
-    char s1[98];
-    char *ptr;
-    int i;
-
-    for (i = 0; i < 98 - 1; i++)
-    {
-        s1[i] = '*';
-    }
-    s1[i] = '\0';
-    printf("%s\n", s1);
-    ptr = _strncpy(s1, "First, solve the problem. Then, write the code\n", 5);
-    printf("%s\n", s1);
-    printf("%s\n", ptr);
-    ptr = _strncpy(s1, "First, solve the problem. Then, write the code\n", 90);
-    printf("%s", s1);
-    printf("%s", ptr);
-    for (i = 0; i < 98; i++)
-    {
-        if (i % 10)
-        {
-            printf(" ");
-        }
-        if (!(i % 10) && i)
-        {
-            printf("\n");
-        }
-        printf("0x%02x", s1[i]);
-    }
-    printf("\n");
-    return (0);
+int a = 0, b = 0;
+while (src[b])
+{
+b++;
+}
+while (a < n && src[a])
+{
+dest[a] = src[a];
+a++;
+}
+while (a < n)
+{
+dest[a] = '\0';
+a++;
+}
+return (dest);
 }
